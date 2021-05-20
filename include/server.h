640d3edff4bf89921166d34638f39990d0e8bb61
Yarin <yarin@pop-os.localdomain>
2021-05-20 23:23:24 +0300

    server typedef

--- include/server.h
+++ include/server.h
@@ -5,10 +5,10 @@
 #include "../include/commands.h"
 
 
-struct server {
+typedef struct server {
     network net;
     struct cmd_tree_node root_commands;
-};
+}server;
 /*
 *this will set the server network to SOCK_STREAM, Provides sequenced, reliable, two-way, connection-
                        		based byte streams.  An out-of-band data transmission mechanism may be supported.
@@ -20,20 +20,20 @@ struct server {
 @param port - the port number for the server,0-2^16, uint16_t.
 @return enum ErrorCode, returns ERROR_SUCCESS if successfully initialized.
 */
-enum ErrorCode server_init(struct server *server_ptr,uint8_t clients_amount,size_t write_buffer_size,size_t read_buffer_size,in_port_t port);
+enum ErrorCode server_init(server *server_ptr,uint8_t clients_amount,size_t write_buffer_size,size_t read_buffer_size,in_port_t port);
 /*
 *this function should only be called to clear the server heap allocations, 
 *this function will clear all dynamic memory allocated from the given server struct.
 @param server_ptr pointer to server struct.
 */
-void server_free_heap(struct server *server_ptr);
+void server_free_heap(server *server_ptr);
 /*
 *this function should only be called to clear the entire server struct, 
 *this will close all its file descriptors and clear alll the heap allocations aswell.
 @param server_ptr pointer to server struct.
 @return enum ErrorCode, returns ERROR_SUCCESS if cleanup succeeded.
 */
-enum ErrorCode server_cleanup(struct server *server_ptr);
+enum ErrorCode server_cleanup(server *server_ptr);
 
 
 
--- src/server.c
+++ src/server.c
@@ -1,16 +1,16 @@
 #include "../include/server.h"
-enum ErrorCode server_init(struct server *server_ptr,uint8_t clients_amount,size_t write_buffer_size,size_t read_buffer_size,in_port_t port) {
+enum ErrorCode server_init(server *server_ptr,uint8_t clients_amount,size_t write_buffer_size,size_t read_buffer_size,in_port_t port) {
     enum ErrorCode error;
     // initialize networking for the server structure.
     if(error = network_init(&server_ptr->net,clients_amount,write_buffer_size,read_buffer_size,port)!=ERROR_SUCCESS) {
         return error;
     }
 }
-void server_free_heap(struct server *server_ptr) {
+void server_free_heap(server *server_ptr) {
     network_free_heap(&server_ptr->net);
     commands_cleanup(&server_ptr->root_commands);
 }
-enum ErrorCode server_cleaup(struct server *server_ptr) {
+enum ErrorCode server_cleaup(server *server_ptr) {
     network_cleanup(&server_ptr->net);
     commands_cleanup(&server_ptr->root_commands);
 }
